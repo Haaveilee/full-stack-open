8658510e9f0648931e382a1506385f626cfdc555
haaveilee <haaveilee@gmail.com>
2024-04-09 23:43:13 +0300

    exercise 2.20

--- part2/countries/src/App.jsx
+++ part2/countries/src/App.jsx
@@ -1,13 +1,13 @@
 import axios from "axios";
 import { useState, useEffect } from 'react';
 
+
 const Country = (props) => {
     const showHandler = (event) => {
         event.preventDefault()
         console.log("clicked", props.id, props.name)
         props.toShow(props.id)
     }
-
     return (
        <div>
            <p>{props.name} <button type="submit" onClick={showHandler}>show</button></p>
@@ -16,19 +16,43 @@ const Country = (props) => {
 }
 
 const CountryToShow = (props) => {
+    const weather_api_key = import.meta.env.VITE_WEATHER_KEY
+    const [weather, setWeather] = useState([])
+
     console.log(props)
+    const weatherApiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + props.countryToShow.capital[0] + "&appid=" + weather_api_key
+
+    const hook = () => {
+        console.log('weather hooked')
+        axios
+            .get(weatherApiUrl)
+            .then(response => {
+                console.log('promise fulfilled')
+                setWeather(response.data)
+                console.log(weather)
+            })
+    }
+    useEffect(hook, []);
+
+    const iconLink = "https://openweathermap.org/img/wn/" + weather.weather[0].icon +"@2x.png"
+    console.log(weather.weather[0].icon)
 
     return (
         <div>
             <h2>{props.countryToShow.name.common}</h2>
             <p>Capital: {props.countryToShow.capital[0]}</p>
             <p>Area: {props.countryToShow.area}</p>
+            <b>Languages:</b>
             <ul>
                 {Object.keys(props.countryToShow.languages).map(key =>
                     <li key={props.countryToShow.languages[key]}>{props.countryToShow.languages[key]}</li>
                 )}
             </ul>
             <img src={props.countryToShow.flags.png} width='10%' />
+            <h3>Weather in {props.countryToShow.capital[0]}</h3>
+            <p>Temperature is: {weather.main.temp} Celsius</p>
+            <img src={iconLink} width='5%' />
+            <p>Wind: {weather.wind.speed} m/s</p>
         </div>
     )
 }
@@ -65,8 +89,8 @@ const Countries = ({filteredCountries, setFilteredCountries}) => {
 }
 
 const App = () => {
-    const [countries, setCountries] = useState([]);
-    const [filteredCountries, setFilteredCountries] = useState([]);
+    const [countries, setCountries] = useState([])
+    const [filteredCountries, setFilteredCountries] = useState([])
 
     const hook = () => {
         console.log('hooked')
@@ -81,14 +105,14 @@ const App = () => {
     useEffect(hook, []);
 
     const filterCountries = (event) => {
-        const textInput = event.target.value;
-        const matchingCountries = [];
+        const textInput = event.target.value
+        const matchingCountries = []
         countries.forEach(country => {
             if (country.name.common.toLowerCase().includes(textInput.toLowerCase())) {
-                matchingCountries.push(country);
+                matchingCountries.push(country)
             }
         })
-        setFilteredCountries(matchingCountries);
+        setFilteredCountries(matchingCountries)
     }
     return (
         <div>
